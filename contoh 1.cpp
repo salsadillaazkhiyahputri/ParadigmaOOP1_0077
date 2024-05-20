c22e8820dba903704296490d5a4089634625f87b
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-20 15:11:30 +0700

    menginisialisasi anggota di public

--- contoh 1.cpp
+++ contoh 1.cpp
@@ -6,5 +6,22 @@ class mahasiswa {
     const int id;
     string nama;
     float nilai;
-    
+
+    public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId),nama(pNama), nilai(pNilai)
+    {//Member Instilization List
+        //Definisi
+    }
+
+    ~mahasiswa() {
+        cout << "Id = " << id << endl;
+        cout << "Nama = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+};
+
+int main () {
+    mahasiswa mhs(12, "Asroni", 90.5);
+
+    return 0;
 }
