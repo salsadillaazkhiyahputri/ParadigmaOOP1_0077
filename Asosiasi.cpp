edef9b531d0af5a5f5ed18d4453d247b94acc7cb
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-20 17:27:55 +0700

    membuat objek dokter dan pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -42,3 +42,12 @@ void pasien:: cetakDokter() {
      }
 cout << endl;
 }
+
+int main(){
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    
+}
